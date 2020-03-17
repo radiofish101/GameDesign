583375313b400688bfb1cf2c79746423b04594f7
radiofish101 <radiofish101@gmail.com>
2020-03-16 20:15:22 -0500

    Create hello world file to learn git commands
    Just a basic hello world that includes a stupid joke

---
+++ .vs/GameDesign/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int test = 13 * 13 - 100;
+	int test2 = 68;
+	if (test == 69) {
+		cout << "69 Nice" << endl;
+	}
+	if (test2 != 69) {
+		cout << " 68 Not nice" << endl;
+	}
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//  git add [file name]
+// git commit 
+// type commit message like it was a command ie: "stop hupming the cat" or "Add hello world file"
+// to end commit messge select the bottom of the console then type ":wq"
+// git push
+// lets say someone else adds to this repo and you want the changes type git pull
